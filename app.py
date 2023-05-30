d117108980cfb0c46e22ed7e25ad97fd4192af13
Anton Strokov <anton.strokoff@gmail.com>
2023-05-30 17:34:40 +0300

    v. 0.1

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True, host='0.0.0.0')
+else:
+    with app.app_context():
+        db.create_all()
+    app.app_context().push()
