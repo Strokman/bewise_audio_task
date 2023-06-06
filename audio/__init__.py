4683eceb4bd51f211e937f45b2574ea7637f3d7f
Anton Strokov <anton.strokoff@gmail.com>
2023-06-06 20:21:46 +0300

    v. 1.2

--- audio/__init__.py
+++ audio/__init__.py
@@ -5,8 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test_task.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI')
 app.config['UPLOAD_FOLDER'] = 'static'
 app.config['ALLOWED_EXTENSIONS'] = ['.wav']
 db = SQLAlchemy(app)
